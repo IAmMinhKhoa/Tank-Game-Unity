b67d9e8686abbb00c5e1dfee9189f2ca2a5527c4
Minh Khoa <88275892+IAmMinhKhoa@users.noreply.github.com>
2023-06-17 00:41:18 +0700

    met

--- Tank Game/Assets/Scrip/Another/Swipe_Menu.cs
+++ Tank Game/Assets/Scrip/Another/Swipe_Menu.cs
@@ -1,8 +1,8 @@
-using System.Threading.Tasks.Dataflow;
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Swipe_Menu : MonoBehaviour
 {
     public GameObject scrollBar;
@@ -12,6 +12,41 @@ public class Swipe_Menu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        pos=new float[TransformBlock.childCount];
+        pos=new float[transform.childCount];
+        float distance = 1f / (pos.Length - 1f);
+        for (int i = 0; i < pos.Length; i++)
+        {
+            pos[i] = distance * i;
+        }
+        if (Input.GetMouseButton(0))
+        {
+            scroll_pos = scrollBar.GetComponent<Scrollbar>().value;
+        }
+        else
+        {
+            for (int i = 0; i < pos.Length; i++)
+            {
+                if (scroll_pos < pos[i]+(distance/2) &&scroll_pos > pos[i] - (distance / 2)){
+                   
+                    scrollBar.GetComponent<Scrollbar>().value = Mathf.Lerp(scrollBar.GetComponent<Scrollbar>().value, pos[i], 0.05f);
+                }
+            }
+        }
+
+        for (int i = 0; i < pos.Length; i++)
+        {
+            if (scroll_pos < pos[i]+(distance/2) && scroll_pos > pos[i] - (distance / 2))
+            {
+                transform.GetChild(i).localScale = Vector2.Lerp(transform.GetChild(i).localScale, new Vector2(1f, 1f), 0.05f);
+                for (int a = 0; a < pos.Length; a++)
+                {
+                    if (a != i)
+                    {
+                   
+                        transform.GetChild(a).localScale = Vector2.Lerp(transform.GetChild(a).localScale, new Vector2(0.8f, 0.8f), 0.05f);
+                    }
+                }
+            }
+        }
     }
 }
